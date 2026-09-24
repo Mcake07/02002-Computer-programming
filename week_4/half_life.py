aeea47cb670fa81228ab9c52e5ebcdb8b8d03c31
Mcake07 <marius.kihlgast@hotmail.com>
2026-09-24 15:31:22 +0200

    half-life-tilføjet

---
+++ week_4/half_life.py
@@ -0,0 +1,11 @@
+R = 0.75
+amount = 6.5
+amount_start = amount
+hour = 0
+
+while amount > (amount_start / 2):
+    hour += 1
+    amount = amount - amount * (1 - R)
+    print(f'The amount after {hour} hours is {amount}')
+
+print(f'Stopped due to the fact that the material has reached its half-life, which is when the amount is at {amount_start / 2} or lower')
